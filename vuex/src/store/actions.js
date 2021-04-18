c64bae965819164e4f2c843f60c39e1990e26bda
Hasinur Rahman <hasinur202@gmail.com>
2021-04-19 00:14:11 +0600

    show product details

--- vuex/src/store/actions.js
+++ vuex/src/store/actions.js
@@ -6,3 +6,11 @@ export const getProducts = ({ commit })=> {
         commit('SET_PRODUCTS', response.data);
     })
 }
+
+
+export const getProduct = ({ commit }, productId) => {
+    axios.get(`http://127.0.0.1:8000/api/products/${productId}`)
+    .then(response => {
+        commit('SET_PRODUCT', response.data);
+    })
+}
--- vuex/src/store/mutations.js
+++ vuex/src/store/mutations.js
@@ -2,3 +2,7 @@
 export const SET_PRODUCTS = (state, products) => {
     state.products = products;
 }
+
+export const SET_PRODUCT = (state, product) => {
+    state.product = product;
+}

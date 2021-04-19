f8e0af5fbdaf710291660e7054f05d7706a474d4
Hasinur Rahman <hasinur202@gmail.com>
2021-04-19 13:42:56 +0600

    smart way use API

--- vuex/src/store/actions.js
+++ vuex/src/store/actions.js
@@ -1,16 +1,16 @@
-import axios from "axios";
+import Product from "../apis/Product";
+import Cart from "../apis/Cart";
 
 export const getProducts = ({ commit })=> {
-    axios.get('http://127.0.0.1:8000/api/products')
-    .then(response => {
+    Product.all().then(response => {
         commit('SET_PRODUCTS', response.data);
     })
 }
 
 
 export const getProduct = ({ commit }, productId) => {
-    axios.get(`http://127.0.0.1:8000/api/products/${productId}`)
-    .then(response => {
+    // axios.get(`http://127.0.0.1:8000/api/products/${productId}`)
+    Product.show(productId).then(response => {
         commit('SET_PRODUCT', response.data);
     })
 }
@@ -18,27 +18,32 @@ export const getProduct = ({ commit }, productId) => {
 export const addProductToCart = ({ commit }, {product, quantity}) => {
     commit('ADD_TO_CART', {product, quantity});
 
-    axios.post('http://127.0.0.1:8000/api/cart',{
+    Cart.store({
         product_id: product.id,
         quantity
-    })
+    });
+
+    // axios.post('http://127.0.0.1:8000/api/cart',{
+    //     product_id: product.id,
+    //     quantity
+    // })
 }
 
 export const getCartItems = ({ commit }) => {
-    axios.get('http://127.0.0.1:8000/api/cart')
-    .then(response => {
+    Cart.all().then(response => {
         commit('SET_CART', response.data);
     })
 }
 
 export const removeProductFromCart = ({ commit }, product) =>{
     commit('REMOVE_PRODUCT_FROM_CART',product);
-    axios.delete(`http://127.0.0.1:8000/api/cart/${product.id}`);
+    Cart.delete(product.id);
+    // axios.delete(`http://127.0.0.1:8000/api/cart/${product.id}`);
 }
 
 export const clearCartItems = ({ commit }) => {
     commit('CLEAR_ALL_CART_ITEMS');
-    axios.delete('http://127.0.0.1:8000/api/cart');
+    Cart.deleteAll();
 }
 
 
